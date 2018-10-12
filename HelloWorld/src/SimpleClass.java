afa575947defc384371585798c231935bb3fdd06
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-12 12:43:43 +0300

    Ex.1 - v1

---
+++ HelloWorld/src/SimpleClass.java
@@ -0,0 +1,10 @@
+// Ex. 1
+
+public class SimpleClass {
+    int defaultInt;
+    char defaultChar;
+    public static void main (String[] args) {
+        SimpleClass sc = new SimpleClass();
+        System.out.println("Int: " + sc.defaultInt + ". Char: " + sc.defaultChar + ".");
+    }
+}
