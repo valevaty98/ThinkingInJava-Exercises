41f3b044c0309ddde45afaef299e6d80d30d0c08
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-17 22:48:00 +0300

    Added Ex.2 - v1

--- 3.Operators/src/initializationAndCleanupChapter5/InitClean.java
+++ 3.Operators/src/initializationAndCleanupChapter5/InitClean.java
@@ -1,11 +1,14 @@
 package initializationAndCleanupChapter5;
 
 public class InitClean {
-    //Ex. 1
-    String str;
+    //Ex. 2
+    String str1 = "Bye", str2;
+    InitClean(String str) {
+        str2 = str;
+    }
     public static void main(String[] args) {
-        InitClean ic = new InitClean();
-        System.out.println(ic.str);
+        InitClean ic = new InitClean("Hello");
+        System.out.println(ic.str1 + " " + ic.str2);
 
     }
 }
