d6115b94ecafbab8713de84cd603990ad95cf813
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-18 00:43:32 +0300

    Added Ex.5 - v1

--- 3.Operators/src/initializationAndCleanupChapter5/InitClean.java
+++ 3.Operators/src/initializationAndCleanupChapter5/InitClean.java
@@ -11,5 +11,33 @@ public class InitClean {
     public static void main(String[] args) {
         InitClean ic1 = new InitClean("howdy?");
         InitClean ic2 = new InitClean();
+
+        Dog dog = new Dog();
+        dog.bark(5);
+        dog.bark('R');
+        dog.bark(false);
+    }
+}
+
+//Ex. 5
+class Dog {
+    void bark(int i) {
+        System.out.println("Bark " + i + " times.");
+    }
+
+    void bark(char c) {
+        for(int i = 0; i < 10; i ++) {
+            System.out.print(c);
+        }
+        System.out.println(c);
+    }
+
+    void bark(boolean b) {
+        if (b) {
+            System.out.println("True to barking");
+        }else {
+            System.out.println("Faaalse!!");
+        }
+
     }
 }
