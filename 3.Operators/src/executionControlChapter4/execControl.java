66fb8723141e261b7db25f7dfd4f2baa6f6ff758
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-17 21:10:50 +0300

    Added Ex. 15 - v1

---
+++ 3.Operators/src/executionControlChapter4/execControl.java
@@ -0,0 +1,10 @@
+package executionControlChapter4;
+
+//Ex. 1
+public class execControl {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
