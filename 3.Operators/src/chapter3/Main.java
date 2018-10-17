94b7a8d141cdcba186b9edb21312282025ca108b
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-17 13:18:24 +0300

    Ex.4 - v1

--- 3.Operators/src/chapter3/Main.java
+++ 3.Operators/src/chapter3/Main.java
@@ -7,10 +7,20 @@ public class Main {
     static void f(Main m) {
         m.f = 9;
     }
+
+    //Ex. 4
+    static float calculateSpeed(float time, float distance) {
+        float speed;
+        speed = distance / time;
+        return speed;
+    }
+
     public static void main(String[] args) {
         Main m1 = new Main();
 
         f(m1);
         System.out.println(m1.f);
+
+        System.out.println("Speed: " + calculateSpeed(10, 500));
     }
 }
