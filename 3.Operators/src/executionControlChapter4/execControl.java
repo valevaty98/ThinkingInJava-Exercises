9c8445a312cc7af1c36e13d12bbd54edad8bb2c0
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-17 21:20:59 +0300

    Added Ex. 16 - v1

--- 3.Operators/src/executionControlChapter4/execControl.java
+++ 3.Operators/src/executionControlChapter4/execControl.java
@@ -1,10 +1,28 @@
 package executionControlChapter4;
 
-//Ex. 1
+import java.util.Random;
+
+//Ex. 2
 public class execControl {
+
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i + " ");
+
+        Random rand = new Random();
+        int[] nums = new int[25];
+
+        nums[0] = rand.nextInt(100);
+        System.out.println(nums[0]);
+        
+        for (int i = 1; i < 25; i++) {
+            nums[i] = rand.nextInt(100);
+            System.out.print(nums[i] + " ");
+            if (nums[i] > nums[i - 1]) {
+                System.out.println("more");
+            } else {
+                System.out.println("less");
+            }
         }
     }
+
+
 }
