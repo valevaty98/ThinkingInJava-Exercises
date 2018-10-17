420cc3708f95c2226761e5f4ac8fc96f44505b67
Vladislav Valevaty <valevaty98@gmail.com>
2018-10-17 22:42:49 +0300

    Ch. 5: Added Ex.1 - v1

---
+++ 3.Operators/src/initializationAndCleanupChapter5/InitClean.java
@@ -0,0 +1,11 @@
+package initializationAndCleanupChapter5;
+
+public class InitClean {
+    //Ex. 1
+    String str;
+    public static void main(String[] args) {
+        InitClean ic = new InitClean();
+        System.out.println(ic.str);
+
+    }
+}
